4edc48f2db45cd33e6e36a13d1aea21902b52189
Sayed Abdul-Aziz <sayed.abdulaziz@swapegypt.app>
2025-10-22 14:32:37 +0300

    fix

--- bookia/lib/features/profile/presentation/widgets/profile_header.dart 23-00-05-382.dart
+++
@@ -1,9 +0,0 @@
-import 'package:bookia/features/profile/presentation/page/profile_screen.dart';
-import 'package:flutter/material.dart';
-
-class ProfileHeader extends StatefulWidget {
-  const ProfileHeader({super.key});
-
-  @override
-  State<ProfileHeader> createState() => _ProfileHeaderState();
-}
